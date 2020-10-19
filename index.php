f912f865569bfcb1f79a9dd59a17ac310fa61db9
My Name <myEmail@example.com>
2020-10-19 20:22:09 +0300

    %

--- index.php
+++ index.php
@@ -6,8 +6,23 @@ require_once 'init.php';
 
 
 $title = 'Главная страница';
+$num = 2; //количество нговостей на странице
 
-$res = mysqli_query($link, "SELECT n.`id`, n.`title`, n.`preview_text`, n.`date`, n.`image`, n.`comment_cnt`, c.`title` AS news_cat FROM `news` n JOIN `category` c ON c.`id` = n.`category_id`");
+$resTotal = mysqli_query($link, "SELECT * FROM `news`");
+$total = mysqli_num_rows($resTotal);
+#pr($total); //количество записей в запрросе
+$totalStr = ceil($total / $num); // общее число страниц
+$page = intval($_GET['page']);  // получение номера страницы из адресной строки  intval() - приводит к числу
+
+if($page <= 0){
+    $page = 1;
+}elseif ($page > $total){
+    $page = $total;
+}
+
+$offset = $page * $num - $numl;
+
+$res = mysqli_query($link, "SELECT n.`id`, n.`title`, n.`preview_text`, n.`date`, n.`image`, n.`comment_cnt`, c.`title` AS news_cat FROM `news` n JOIN `category` c ON c.`id` = n.`category_id` LIMIT 1, $num");
 $arNews = mysqli_fetch_all($res, MYSQLI_ASSOC);
 
 #pr($arNews);
